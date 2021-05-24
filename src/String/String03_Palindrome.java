c370af2f705a6f128ba56701523ed0773404a742
unknown <vishwanathchaudhary2019@gmail.com>
2021-05-24 22:11:53 +0530

    ,

--- src/String/String03_Palindrome.java
+++ src/String/String03_Palindrome.java
@@ -7,8 +7,13 @@ public class String03_Palindrome {
 		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the string to check palindrome: ");
-		String str, rev = "";
-		str = input.nextLine();
+		String str = input.next();
+		method1(str);
+		method2(str); // By reversing the String using recurssion
+	}
+
+	private static void method1(String str) {
+		String rev = "";
 		for (int i = 0; i < str.length(); i++) {
 			rev = str.charAt(i) + rev;
 		}
@@ -17,6 +22,18 @@ public class String03_Palindrome {
 		} else {
 			System.out.println("Not palindrome " + rev);
 		}
+	}
+	
 
+	private static void method2(String input) {
+		String rev = reverse(input);
+		System.out.println(rev.equals(input)?"Palindrome":"Not Palindrome");
+	}
+
+	private static String reverse(String str) {
+		if(str.length()==1)
+			return str;
+		else
+			return reverse(str.substring(1)) + str.charAt(0);			
 	}
 }
